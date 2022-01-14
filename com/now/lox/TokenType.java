312ca2472d80ea05c1f074d756aaae7780f4bf85
no-weller <github@blackthorn-hill.de>
2022-01-14 15:43:30 +0100

    feat(Scanner.java): :sparkles: Detect single character lexemes, errors and operators
    This introduces the detection of single character lexemes, operators and lexical errors. Also introduces several helper functions to match those characters and add them to the list.

--- com/now/lox/TokenType.java
+++ com/now/lox/TokenType.java
@@ -3,7 +3,7 @@ package com.now.lox;
 enum TokenType {
   // Single-character tokens
   LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-  COMMA, DOT, PLUS, SEMICOLON, SLASH, STAR,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
   // One or two character tokens
   BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
