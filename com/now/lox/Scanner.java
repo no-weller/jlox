db3fd6361a0867c3780a4d7da74217775b56ea40
no-weller <github@blackthorn-hill.de>
2022-01-14 17:12:03 +0100

    feat(Scanner.java): :sparkles: Detect identifiers
    Introduces the reserved keywords for lox

--- com/now/lox/Scanner.java
+++ com/now/lox/Scanner.java
@@ -15,6 +15,28 @@ class Scanner {
   private int current = 0;
   private int line = 1;
 
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",     AND);
+    keywords.put("class",   CLASS);
+    keywords.put("else",    ELSE);
+    keywords.put("false",   FALSE);
+    keywords.put("for",     FOR);
+    keywords.put("fun",     FUN);
+    keywords.put("if",      IF);
+    keywords.put("nil",     NIL);
+    keywords.put("or",      OR);
+    keywords.put("print",   PRINT);
+    keywords.put("return",  RETURN);
+    keywords.put("super",   SUPER);
+    keywords.put("this",    THIS);
+    keywords.put("true",    TRUE);
+    keywords.put("var",     VAR);
+    keywords.put("while",   WHILE);
+  }
+
   Scanner(String source) {
     this.source = source;
   }
@@ -75,11 +97,21 @@ class Scanner {
       default:
         if(isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+
+    addToken(type);
   }
 
   // Helper Methods
@@ -120,6 +152,16 @@ class Scanner {
 
   private boolean isDigit(char c) {
     return c >= '0' && c <= '9';
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   // Literals
